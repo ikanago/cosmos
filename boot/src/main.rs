0fccab1402baa1fa386b4191ab7e23df771f0332
ikanago <ikanago-dev@protonmail.com>
2022-06-02 11:41:36 +0000

    Build UEFI binary from Rust
    Currently, bootloader is invoked from UEFI shell by executing "boot" command.

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+#![feature(abi_efiapi)]
+
+use core::fmt::Write;
+use uefi::prelude::entry;
+use uefi::table::{Boot, SystemTable};
+use uefi::{Handle, Status};
+
+#[entry]
+fn efi_main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).expect("Failed to initialize utilities");
+    system_table.stdout().reset(false).unwrap();
+    writeln!(system_table.stdout(), "Hello, UEFI!").unwrap();
+
+    loop {}
 }
--- run_qemu.sh
+++ run_qemu.sh
@@ -5,15 +5,18 @@ set -eu
 EFI_BINARY=$1
 
 qemu-img create -f raw disk.img 200M
-mkfs.fat -n 'cosmos' -s 2 -f 2 -R 32 -F32 disk.img
+mkfs.fat -n 'COSMOS' -s 2 -f 2 -R 32 -F32 disk.img
 mkdir -p mnt
 sudo mount -o loop disk.img mnt
 sudo mkdir -p mnt/EFI/BOOT
 sudo cp "$EFI_BINARY" mnt/EFI/BOOT/"$EFI_BINARY"
 sudo umount mnt
 
+# Press <ESC> on boot
+# Boot Maintenance Manager > Boot From File > COSMOS > EFI/BOOT/BOOT.EFI
 qemu-system-x86_64 \
-    -drive if=pflash,file=/home/vscode/osbook/devenv/OVMF_CODE.fd \
-    -drive if=pflash,file=/home/vscode/osbook/devenv/OVMF_VARS.fd \
-    -hda disk.img \
-    -vnc :0
+    -drive if=pflash,format=raw,readonly,file=/home/vscode/osbook/devenv/OVMF_CODE.fd \
+    -drive if=pflash,format=raw,file=/home/vscode/osbook/devenv/OVMF_VARS.fd \
+    -drive if=ide,format=raw,media=disk,index=0,file=disk.img \
+    -monitor stdio
+    # -vnc :0
